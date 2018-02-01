55b29f0e33a431a5c28d51231ee9efcb74296bbf
kolbasik <sergii.kolbasin@albelli.com>
2018-02-01 11:28:44 +0100

    used the Microsoft.Extensions.CommandLineUtils in CLI

--- NuGetGraph.CLI/Program.cs
+++ NuGetGraph.CLI/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Xml.Linq;
+using Microsoft.Extensions.CommandLineUtils;
 using NuGetGraph.Core;
 
 namespace NuGetGraph.CLI
@@ -12,39 +14,79 @@ namespace NuGetGraph.CLI
     {
         private static void Main(params string[] args)
         {
-            var cli = Options.Default(o =>
+            var app = new CommandLineApplication
             {
-                o.Input.Path = args.ElementAtOrDefault(0) ?? ".";
-                o.Input.ExcludeConfigs.Add(".TEST");
-                o.Output.Type = Options.OutputTypes.File;
-                o.Output.OpenFile = true;
+                Name = Path.GetFileName(Environment.GetCommandLineArgs()[0]),
+                FullName = "The NuGet Graph CLI",
+            };
+            app.HelpOption("-?|-h|--help");
+
+            app.Command("version", command =>
+            {
+                command.Description = "show version information";
+                command.HelpOption("-?|-h|--help");
+
+                command.OnExecute(() =>
+                {
+                    var assembly = typeof(Program).Assembly;
+                    var version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+                                  ?? assembly.GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version;
+                    Console.WriteLine(version);
+                    return 0;
+                });
             });
 
-            var nugetGraph = NuGets.GetGraph(cli.ToNuGetGraphOptions());
-            if (cli.Graph.Simplify)
+            app.Command("graph", command =>
             {
-                nugetGraph.Simplify();
-            }
+                command.Description = "build a NuGet graph";
+                command.HelpOption("-?|-h|--help");
 
-            var dgmlGraph = nugetGraph.ToDgmlGraph();
-            if (cli.Graph.UseStyles)
-            {
-                dgmlGraph.UseStyles();
-            }
-
-            var dgml = dgmlGraph.ToDgml(cli.ToDgmlOptions());
-            if (cli.Output.Type.HasFlag(Options.OutputTypes.Console))
-            {
-                Console.WriteLine(dgml);
-            }
-            if (cli.Output.Type.HasFlag(Options.OutputTypes.File))
-            {
-                File.WriteAllText(cli.Output.FilePath, dgml);
-                if (cli.Output.OpenFile)
+                command.OnExecute(() =>
                 {
-                    Process.Start(new ProcessStartInfo("explorer.exe", cli.Output.FilePath));
-                }
-            }
+                    var cli = Options.Default(o =>
+                    {
+                        o.Input.Path = args.ElementAtOrDefault(0) ?? ".";
+                        o.Input.ExcludeConfigs.Add(".TEST");
+                        o.Output.Type = Options.OutputTypes.File;
+                        o.Output.OpenFile = true;
+                    });
+
+                    var nugetGraph = NuGets.GetGraph(cli.ToNuGetGraphOptions());
+                    if (cli.Graph.Simplify)
+                    {
+                        nugetGraph.Simplify();
+                    }
+
+                    var dgmlGraph = nugetGraph.ToDgmlGraph();
+                    if (cli.Graph.UseStyles)
+                    {
+                        dgmlGraph.UseStyles();
+                    }
+
+                    var dgml = dgmlGraph.ToDgml(cli.ToDgmlOptions());
+                    if (cli.Output.Type.HasFlag(Options.OutputTypes.Console))
+                    {
+                        Console.WriteLine(dgml);
+                    }
+                    if (cli.Output.Type.HasFlag(Options.OutputTypes.File))
+                    {
+                        File.WriteAllText(cli.Output.FilePath, dgml);
+                        if (cli.Output.OpenFile)
+                        {
+                            Process.Start(new ProcessStartInfo("explorer.exe", cli.Output.FilePath));
+                        }
+                    }
+                    return 0;
+                });
+            });
+
+            app.OnExecute(() =>
+            {
+                app.ShowHelp();
+                return 0;
+            });
+
+            app.Execute(args);
         }
 
         private sealed class Options
